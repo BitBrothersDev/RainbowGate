e08ff06b0612c1f41bc5f1c4f5702f7327b17257
Orest Kostiuk <okostiuk@assurance.com>
2023-06-01 15:44:34 +0200

    added after sign out path

--- app/controllers/rainbow_gate/application_controller.rb
+++ app/controllers/rainbow_gate/application_controller.rb
@@ -3,5 +3,9 @@ module RainbowGate
     def after_sign_in_path_for(resource)
       main_app.root_path
     end
+
+    def after_sign_out_path_for(resource)
+      main_app.root_path
+    end
   end
 end
