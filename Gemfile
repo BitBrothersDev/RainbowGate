cdf498ebafb764e4ed3fd73fbfc713977c487a09
Orest Kostiuk <okostiuk@assurance.com>
2023-06-01 18:03:19 +0200

    added code for google auth

--- Gemfile
+++ Gemfile
@@ -15,3 +15,12 @@ gem 'letter_opener'
 
 # Start debugger with binding.b [https://github.com/ruby/debug]
 # gem "debug", ">= 1.0.0"
+gem 'devise'
+
+gem 'omniauth'
+
+gem 'omniauth-google-oauth2'
+
+gem 'omniauth-rails_csrf_protection'
+
+gem "dotenv-rails"
--- app/controllers/rainbow_gate/users/omniauth_callbacks_controller.rb
+++ app/controllers/rainbow_gate/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class RainbowGate::Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class PluginAuthentication::Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
@@ -8,6 +8,21 @@ class RainbowGate::Users::OmniauthCallbacksController < Devise::OmniauthCallback
   # def twitter
   # end
 
+  # skip_before_action :verify_authenticity_token
+
+  def google_oauth2
+    binding.pry
+    user = PluginAuthentication::User.from_omniauth(auth)
+
+    if user.present?
+      sign_out_all_scopes
+      flash[:success] = 'Success login'
+      sign_in_and_redirect user, event: :authentication
+    else
+      flash[:alert] = "Login fail #{auth.info.email}"
+      redirect_to new_plugin_authentication_user_session_path
+    end
+  end
   # More info at:
   # https://github.com/heartcombo/devise#omniauth
 
@@ -27,4 +42,10 @@ class RainbowGate::Users::OmniauthCallbacksController < Devise::OmniauthCallback
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
+
+  private
+
+  def auth
+    @auth ||= request.env['omniauth.auth']
+  end
 end
