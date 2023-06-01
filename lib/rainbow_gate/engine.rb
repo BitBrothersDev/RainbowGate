ec26d1443e2bb2e76069c366df9f646dd41d22e7
Orest Kostiuk <okostiuk@assurance.com>
2023-06-01 15:04:36 +0200

    added devise on load to engine.rb

--- lib/rainbow_gate/engine.rb
+++ lib/rainbow_gate/engine.rb
@@ -1,5 +1,11 @@
 module RainbowGate
   class Engine < ::Rails::Engine
     isolate_namespace RainbowGate
+
+    initializer 'plugin_authentication.action_controller' do |app|
+      ActiveSupport.on_load :action_controller do
+        include Devise::Controllers::Helpers
+      end
+    end
   end
 end
