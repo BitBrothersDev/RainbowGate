287ef0c6e68c17bb1727dc7d4823c465b06d2957
Orest Kostiuk <okostiuk@assurance.com>
2023-06-01 18:37:37 +0200

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 RainbowGate::Engine.routes.draw do
-  devise_for :users, class_name: 'RainbowGate::User', module: "rainbow_gate/users",
-             controllers: {
-                registrations: 'rainbow_gate/users/registrations',
-                sessions: 'rainbow_gate/users/sessions',
-                omniauth_callbacks: 'rainbow_gate/users/omniauth_callbacks'
-              }
+  devise_for :users, class_name: 'RainbowGate::User', module: "rainbow_gate/users"
 end
